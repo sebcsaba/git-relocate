14abfe376dd21726631a41eeb072c8b691e0cd4d
sebcsaba <sebcsaba@gmail.com>
2014-12-25 11:54:46 +0100

    add GitRunner basic version

---
+++ src/main/java/hu/sebcsaba/gitrelocate/GitRunner.java
@@ -0,0 +1,66 @@
+package hu.sebcsaba.gitrelocate;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GitRunner {
+	
+	private final CmdLineTool cmdLine;
+	
+	public GitRunner(CmdLineTool cmdLine) {
+		this.cmdLine = cmdLine;
+	}
+	
+	public CommitID getCommitId(String anyGitCommitRef) {
+		return new CommitID(gitString("rev-parse", anyGitCommitRef).trim());
+	}
+	
+	public List<CommitID> getCommitParentIds(CommitID commitId) {
+		return getCommitIDList(gitStringList("log", "--pretty=%P", "-n", "1", commitId.toString()));
+	}
+	
+	public List<String> getTagNames() {
+		return gitStringList("tag");
+	}
+	
+	public List<String> getBranchNames() {
+		List<String> result = gitStringList("branch");
+		result.remove("*");
+		return result;
+	}
+	
+	private String gitString(String... params) {
+		try {
+			return cmdLine.getString(unshift("git", params));
+		}
+		catch (IOException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+	
+	private List<String> gitStringList(String... params) {
+		try {
+			return cmdLine.getStringList(unshift("git", params));
+		}
+		catch (IOException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+	
+	private List<CommitID> getCommitIDList(List<String> ids) {
+		List<CommitID> result = new ArrayList<CommitID>(ids.size());
+		for (String id : ids) {
+			result.add(new CommitID(id));
+		}
+		return result;
+	}
+	
+	private String[] unshift(String first, String[] rest) {
+		String[] result = new String[rest.length+1];
+		result[0] = first;
+		System.arraycopy(rest, 0, result, 1, rest.length);
+		return result;
+	}
+
+}
